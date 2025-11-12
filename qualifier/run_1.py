3ce947f367f384b0549cbe36a77ff17fc2d2f1f1
graysoncornwall <grayson.cornwall@swampbots.org>
2025-11-11 19:20:52 -0500

    Final changes for qualifier

--- qualifier/run_1.py
+++ qualifier/run_1.py
@@ -9,30 +9,30 @@ Engineer: Who runs this mission?
 Authors: Who coded this run (name all)
 '''
 def R1_run():
-    robot.settings(straight_acceleration=550,straight_speed=600)
+    robot.settings(straight_acceleration=450,straight_speed=600)
     robot.use_gyro(True)
 
     # Square up against wall to start
     square_up()
 
-    #motorC.run_angle(3000,15)
-    # lower lever to start at 0
-    motorC.run_angle(3000,-60)
     # raise lever to hit brush
-    robot.straight(3 * wheel_diameter * 3.14159)
+    motorC.run_angle(3000,-60)
     # Go towards brush and hit one side
-    robot.turn(15)
+    robot.straight(3 * wheel_diameter * 3.14159)
     # slight turn towards mineshaft
-    robot.straight(0.9 * wheel_diameter * 3.14159)
+    robot.turn(15)
     # slight movement to get away from brush
-    robot.turn(75)
+    robot.straight(0.9 * wheel_diameter * 3.14159)
     # finish turn towards mineshaft
-    robot.settings(straight_acceleration=700,straight_speed=700)
+    robot.turn(75)
+    robot.settings(straight_acceleration=450,straight_speed=450)
 
-    motorC.run_angle(3000, 50)
     # lower lever
-    robot.straight(2 * wheel_diameter * 3.14159)
+    motorC.run_angle(3000, 50)
     # go towards miner
+    robot.straight(2 * wheel_diameter * 3.14159)
+
+    # 
     robot.settings(straight_acceleration=150,straight_speed=150)
 
     # lift mineshaft
@@ -47,41 +47,58 @@ def R1_run():
 
     motorD.run_angle(3000, 90)
 
-    robot.settings(straight_acceleration=550,straight_speed=600)
-
-    robot.straight(-1.6 * wheel_diameter * 3.14159)
     # back up towards map reveal
+    robot.settings(straight_acceleration=550,straight_speed=600)
+    # adjust lever to reveal map 
+    robot.straight(-1.55 * wheel_diameter * 3.14159)
     motorC.run_angle(3000,-110)
-    # adjust lever to reveal map
-    robot.turn(-90)
+   
     # turn to begin map approach
-    robot.straight(-0.75 * wheel_diameter * 3.14159)
+    robot.turn(-90)
     # back up to adjust entry angle
-    robot.turn(-45)
+    robot.straight(-0.85 * wheel_diameter * 3.14159)
     # turn towards map mission
-    #motorC.run_angle(3000,-30)
+    robot.turn(-45)
     # adjust lever arm to get map reveal
     robot.settings(straight_acceleration=150,straight_speed=150)
 
-    robot.straight(2.1 * wheel_diameter * 3.14159)
     # move towards map reveal
-    motorC.run_angle(150,120)
+    robot.straight(2.1 * wheel_diameter * 3.14159)
     # lift map reveal
-    robot.straight(-0.9 * wheel_diameter * 3.14159)
+    motorC.run_angle(150,120)
     # back up from map reveal
-    robot.turn(45)
-    # turn towards brush
-    motorC.run_angle(150,60)
-    # adjust lever for brush
-    robot.settings(straight_acceleration=550,straight_speed=600)
-    robot.straight(-1 * wheel_diameter * 3.14159)
-    robot.straight(1.5 * wheel_diameter * 3.14159)
     robot.straight(-0.9 * wheel_diameter * 3.14159)
+    # turn towards brush
+    robot.turn(45)
+    # back up from map reveal
+    robot.straight(-0.2 * wheel_diameter * 3.14159)
+
+    motorC.run_angle(3000,80)
+
+    robot.settings(straight_acceleration=450,straight_speed=550)
+    
+    robot.straight(0.4 * wheel_diameter * 3.14159)
+
+    robot.straight(-0.8 * wheel_diameter * 3.14159)
+    
+
+    motorC.run_angle(150,-60)
+    robot.straight(-3.5 * wheel_diameter * 3.14159)
+
+    '''
+    # adjust lever for brush
+    motorC.run_angle(150,60)
+    robot.settings(straight_acceleration=450,straight_speed=600)
+
+    #robot.straight(1.5 * wheel_diameter * 3.14159)
+    #robot.straight(-1 * wheel_diameter * 3.14159)
+    #robot.straight(0.35 * wheel_diameter * 3.14159)
     # back up and activate brush
     motorC.run_angle(100,-40, then=Stop.HOLD)
     # lift up brush
-    robot.straight(-3.3 * wheel_diameter * 3.14159)
+    robot.straight(-3.4 * wheel_diameter * 3.14159)
     # back up to red home
+    '''
 
 # If we're running ONLY this run (without the menu)
 if __name__ == '__main__':
--- qualifier/run_2.py
+++ qualifier/run_2.py
@@ -1,4 +1,5 @@
-from tadpoleBot import robot, rotation, square_up, wheel_diameter
+from tadpoleBot import robot, rotation, square_up, wheel_diameter, motorC, wait
+from pybricks.parameters import Stop
 
 '''
 Run 2 - Describe what it does
@@ -14,25 +15,35 @@ def R2_run():
 
     # go towards sand
     robot.straight(2.25 * wheel_diameter * 3.14159)
+    #change speed
+    robot.settings(straight_acceleration=350,straight_speed=350)
     # pull back lever
     robot.straight(-1.2 * wheel_diameter * 3.14159)
     # turn to boat lever
     robot.turn(-45)
-
+    # go towards boat lever
     robot.straight(1 * wheel_diameter * 3.14159)
-
+    # go forward to the boat lever 
     robot.turn(35)
-    #push boat lever
+    # push boat lever
     robot.straight(0.9 * wheel_diameter * 3.14159)
-#turn away from boat
+    # drop off flag
+    motorC.run_angle(300,-120, then=Stop.HOLD)
+    # wait so we can let go of flag
+    wait(500)
+    # raise lever back up
+    motorC.run_angle(400,120, then=Stop.HOLD)
+    #adjust speed back to normal
+    robot.settings(straight_acceleration=660,straight_speed=660)
+    # turn away from boat
     robot.turn(-30)
-#go past boat 
+    # go past boat 
     robot.straight(1.5 * wheel_diameter * 3.14159)
-#turn towards lift
+    # turn towards lift
     robot.turn(90)
-#go towards lift    
+    # go towards lift    
     robot.straight(1.2 * wheel_diameter * 3.14159)
-#activate lift lever
+    # activate lift lever
     robot.turn(25)
 
     robot.turn(-25)
@@ -42,28 +53,27 @@ def R2_run():
     robot.turn(-25)
 
     robot.turn(25)
- #finish activating lever
+    # finish activating lever
     robot.turn(-25)
-
-#back away from lift
+    # back away from lift
     robot.straight(-1.5 * wheel_diameter * 3.14159)
-    #turn towards scale
+    # turn towards scale
     robot.turn(-95)
-    #go towards scale    
+    # go towards scale    
     robot.straight(3.8 * wheel_diameter * 3.14159)
-#turn towards table lever
+    # turn towards table lever
     robot.turn(90)
- #push table lever
-    robot.straight(1.2 * wheel_diameter * 3.14159)
-# back up from table lever
+    # push table lever
+    robot.straight(1 * wheel_diameter * 3.14159)
+    # back up from table lever
     robot.straight(-0.2 * wheel_diameter * 3.14159)
-#turn away from table lever
+    # turn away from table lever
     robot.turn(-30)
-   #head towards home   
+    #head towards home   
     robot.straight(0.75 * wheel_diameter * 3.14159)
- #turn towards home
+    # turn towards home
     robot.turn(50)
- #finish heading to blue home  
+    #finish heading to blue home  
     robot.straight(3.95 * wheel_diameter * 3.14159)
  
 
