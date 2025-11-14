0bd6fb55045a85933defcefa01ca71aa022c68a1
graysoncornwall <grayson.cornwall@swampbots.org>
2025-11-13 21:05:28 -0500

    Tweak run 4

--- qualifier/run_4.py
+++ qualifier/run_4.py
@@ -26,21 +26,29 @@ def R4_run():
     motorD.run_angle(400,160, then=Stop.HOLD)
     motorD.run_angle(400,-162, then=Stop.HOLD)
     motorD.run_angle(400,160, then=Stop.HOLD)
+    # make the 
     robot.settings(straight_acceleration=400, straight_speed=500)
     # leave the silo
     robot.straight(-0.3 * rotation) 
     # turn toward the market wares 
     robot.turn(-80)
     # Head toward the market wares
-    robot.straight(1.8 * rotation)
-    # Lift the market table
+    robot.straight(1.7 * rotation)
+
+    # turn to 
     robot.turn(-45)
-    robot.straight(3.1 * rotation)
+    # Lift the market table 
+    robot.settings(straight_acceleration=200, straight_speed=200)
+    # adjust the speed to go slower
+    robot.straight(2.75 * rotation)
+    # change the speed back to normal
+    robot.settings(straight_acceleration=400, straight_speed=500)
     # turn to solve the seal
-    robot.turn(92)
+    robot.turn(87)
     # drive toward the seal
-    robot.straight(2.1 * rotation)
-    motorD.run_angle(2000, -200)
+    robot.straight(2.4 * rotation)
+    # hit the seal
+    motorD.run_angle(2000, -190)
     # leave the seal
     robot.straight(-0.5 * rotation)
     # turn to go home
