194a9dbd471347c21d2293ec7fa59697dfd26fc4
Tanner Farris <tanner.farris@swampbots.org>
2025-11-21 16:32:03 -0500

    WIP: smoother run 1

--- regionals/run_1.py
+++ regionals/run_1.py
@@ -1,4 +1,4 @@
-from tadpoleBot import robot, rotation, square_up, motorC, wait, multitask, run_task, run_motorC, drive_straight, turn
+from tadpoleBot import robot, rotation, square_up, motorC, wait, multitask, run_task, run_motorC, drive_straight, turn, arc
 
 # Basic robot settings
 robot.settings(straight_acceleration=400,straight_speed=600)
@@ -17,22 +17,11 @@ async def R1_run():
     await square_up()
 
     # Raise lever to avoid hitting brush and go towards brush
-    await multitask(run_motorC(3000, gear_ratio * 60), drive_straight(3))
-    # Slight turn towards mineshaft
-    await turn(15)
-    # Slight movement to get away from brush
-    await drive_straight(0.9)
-    # Finish turn towards mineshaft and lower lever
-    await multitask(turn(75), run_motorC(3000, gear_ratio * -50))
-
-    # Lower the speed for more precise movement
-    robot.settings(straight_acceleration=450, straight_speed=450)
-
-    # Go towards mineshaft
-    await drive_straight(2)
-
-    # Lower the speed even more for precise alignment
-    robot.settings(straight_acceleration=150, straight_speed=150)
+    await multitask(run_motorC(3000, gear_ratio * 60), drive_straight(2))
+    # Arc toward mineshaft and lower arm
+    await multitask(arc(320, 90), run_motorC(50, gear_ratio * -70))
+    # Get a little closer to the mineshaft
+    await drive_straight(0.5)
     
     # Set some variables to help tweak adjustments easier
     total_rotation = gear_ratio * 80
@@ -50,6 +39,9 @@ async def R1_run():
     # Speed the robot back up again
     robot.settings(straight_acceleration=550, straight_speed=600)
 
+    await arc(225, -110)
+
+    '''
     # Back up towards map reveal & adjust lever to lift top soil
     await multitask(drive_straight(-1.55), run_motorC(3000, gear_ratio * 110))
    
@@ -89,7 +81,7 @@ async def R1_run():
 
     # Head home
     await drive_straight(-3.5)
-
+    '''
 
 # If we're running ONLY this run (without the menu)
 if __name__ == '__main__':
