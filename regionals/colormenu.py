0a2bd20a7e965cfb165c7091b0865b6aec5bc9ce
graysoncornwall <grayson.cornwall@swampbots.org>
2025-12-08 18:16:58 -0500

    Tweak Color Menu

--- regionals/colormenu.py
+++ regionals/colormenu.py
@@ -4,7 +4,7 @@ from pybricks.parameters import Port, Color, Button
 from pybricks.tools import wait
 from tadpoleBot import robot, Tanner, Grayson
 
-# from A_fn_diagnostics import run_diagnostics
+# Import your existing run files
 from run_1 import R1_run
 from run_2 import R2_run
 from run_3 import R3_run
@@ -14,41 +14,52 @@ from run_5 import R5_run
 hub = PrimeHub()
 color_sensor = ColorSensor(Port.E)
 
-# Normally, the center button stops the program. But we want to use the
-# center button for our menu. So we can disable the stop button.
+# Disable system stop button (optional)
 hub.system.set_stop_button(None)
 
+
+
 # ----------------------
-# Program Functions
+# Program functions
 # ----------------------
 def run_program_red():
-    print("RUN RED")
     hub.light.on(Color.RED)
+    hub.display.number(1)
+    wait(200)  # show number briefly before starting run
     R1_run()
-    hub.light.off()
-
-def run_program_blue():
-    print("RUN BLUE")
-    hub.light.on(Color.BLUE)
-    R3_run()
+    hub.display.clear()
     hub.light.off()
 
 def run_program_yellow():
-    print("RUN YELLOW")
     hub.light.on(Color.YELLOW)
+    hub.display.number(2)
+    wait(200)
     R2_run()
+    hub.display.clear()
     hub.light.off()
 
-def run_program_white():
-    print("RUN WHITE")
-    hub.light.on(Color.WHITE)
-    R5_run()
+def run_program_blue():
+    hub.light.on(Color.BLUE)
+    hub.display.number(3)
+    wait(200)
+    R3_run()
+    hub.display.clear()
     hub.light.off()
 
 def run_program_green():
-    print("RUN GREEN")
     hub.light.on(Color.GREEN)
+    hub.display.number(4)
+    wait(200)
     R4_run()
+    hub.display.clear()
+    hub.light.off()
+
+def run_program_white():
+    hub.light.on(Color.WHITE)
+    hub.display.number(5)
+    wait(200)
+    R5_run()
+    hub.display.clear()
     hub.light.off()
 
 # ----------------------
@@ -59,61 +70,46 @@ def main_menu():
     hub.light.on(Color.WHITE)
 
     while True:
+        # Detect color
         detected_color = color_sensor.color()
 
-        # Select programs by color
         if detected_color == Color.RED:
-            print("Red selected")
             selected_program = run_program_red
             hub.light.on(Color.RED)
-
-        elif detected_color == Color.BLUE:
-            print("Blue selected")
-            selected_program = run_program_blue
-            hub.light.on(Color.BLUE)
-
         elif detected_color == Color.YELLOW:
-            print("Yellow selected")
             selected_program = run_program_yellow
             hub.light.on(Color.YELLOW)
-
+        elif detected_color == Color.BLUE:
+            selected_program = run_program_blue
+            hub.light.on(Color.BLUE)
+        elif detected_color == Color.GREEN:
+            selected_program = run_program_green
+            hub.light.on(Color.GREEN)
         elif detected_color == Color.WHITE:
-            print("White selected")
             selected_program = run_program_white
             hub.light.on(Color.WHITE)
 
-        elif detected_color == Color.GREEN:
-            print("Green selected")
-            selected_program = run_program_green
-            hub.light.on(Color.GREEN)
+        hub.system.set_stop_button(Button.CENTER)
 
-        # If center button is pressed, run selected program
-        if Button.CENTER in hub.buttons.pressed():
-            if selected_program is not None:
+        hub.light.on(Color.BROWN)
 
-                # ---- IMPORTANT ----
-                # Wait until the button is released to avoid SystemExit
-                while Button.CENTER in hub.buttons.pressed():
-                    wait(10)
-
-                hub.system.set_stop_button(Button.CENTER)
-
-                hub.light.on(Color.BROWN)
-
-                selected_program()
+        selected_program()
     
-                hub.system.set_stop_button(None)
-                hub.light.on(Color.WHITE)
-                selected_program = None  # require new color selection
+        hub.system.set_stop_button(None)
+        hub.light.on(Color.WHITE)
+        selected_program = None  # require new color selection
+        # Stop everything if BLUETOOTH pressed
 
         if Button.BLUETOOTH in hub.buttons.pressed():
             robot.stop()
             Tanner.stop()
             Grayson.stop()
-            hub.speaker.beep(frequency=1, duration = 50)
+            hub.speaker.beep(600, 50)
+            break
 
         wait(50)
 
-
+# ----------------------
 # Start menu
+# ----------------------
 main_menu()
